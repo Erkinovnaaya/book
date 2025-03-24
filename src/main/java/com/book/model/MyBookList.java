ed96c1b478efe65d31c5917c8e45122941e63626
Erkinovnaaya <ayazhan0e@gmail.com>
2025-03-24 12:13:23 +0500

    add Date annotation

--- src/main/java/com/book/model/MyBookList.java
+++ src/main/java/com/book/model/MyBookList.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@Data
 @Entity
 @Table(name="MyBooks")
 public class MyBookList {
@@ -14,39 +14,5 @@ public class MyBookList {
     private String name;
     private String author;
     private String price;
-    public MyBookList() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    public MyBookList(int id, String name, String author, String price) {
-        super();
-        this.id = id;
-        this.name = name;
-        this.author = author;
-        this.price = price;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-    public String getPrice() {
-        return price;
-    }
-    public void setPrice(String price) {
-        this.price = price;
-    }
+
 }
